dee7092261137d656b9ba6c0ed8687be22755f73
Keith Potempa <keithrpotempa@gmail.com>
2020-01-17 14:02:20 -0600

    finished practice My Pet This

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const bella = {
     ],
     pronoun: "She",
     age: 10,
+    favoriteToys: [],
     meow: function() {
         console.log(`${this.name} meows loudly`)
     },
@@ -19,9 +20,20 @@ const bella = {
     zoom: function(){
         console.log(`${this.name} has the zooms!`);
         console.log(`${this.pronoun} zooms past you this way, then zooms past you that way.`);
+    },
+    play: function(toy) {
+        console.log(`${this.name} plays with ${toy}.`)
+        if (toy.includes("real fur")) {
+            console.log(`${this.name} decided ${toy} is a new favorite.`);
+            this.favoriteToys.push(toy);
+        } else {
+            console.log(`${this.name} doesn't seem to care for ${toy}.`)
+        }
     }
 }
 
 bella.meow();
 bella.heelClick();
 bella.zoom();
+bella.play("plastic mouse");
+bella.play("real fur mouse");
